7bea710b2b1305d1961c1c566aaa743a1124ec5f
Azu機 <37403134+Azuki-Azusa@users.noreply.github.com>
2019-05-06 01:04:33 +0800

    Add files via upload

---
+++ 16340008_蔡梓珩_HW2/src/main4.cpp
@@ -0,0 +1,190 @@
+﻿#include <glad/glad.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(ourColor, 1.0);"
+"}\n\0";
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(600, 600, "CG_HW2", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	
+
+	float vertices[] = {
+	-0.75f, -0.25f, 0.0f, 1.0f,  1.0f, 1.0f,
+	-0.75f, 0.25f, 0.0f, 1.0f,  1.0f, 1.0f,
+	-0.25f, -0.25f, 0.0f, 1.0f,  1.0f, 1.0f,
+	-0.25f, 0.25f, 0.0f, 1.0f,  1.0f, 1.0f,
+	0.25f, -0.25f, 0.0f, 1.0f,  1.0f, 1.0f,
+	0.25f, 0.25f, 0.0f, 1.0f,  1.0f, 1.0f,
+	0.75f, -0.25f, 0.0f, 1.0f,  1.0f, 1.0f,
+	0.75f, -0.15f, 0.0f, 1.0f,  1.0f, 1.0f,
+	0.75f, -0.05f, 0.0f, 1.0f,  1.0f, 1.0f,
+	0.75f, 0.05f, 0.0f, 1.0f,  1.0f, 1.0f,
+	0.75f, 0.15f, 0.0f, 1.0f,  1.0f, 1.0f,
+	0.75f, 0.25f, 0.0f, 1.0f,  1.0f, 1.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		1, 2, 3
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindVertexArray(VAO);
+
+	
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	const char* glsl_version = "#version 330";
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	ImVec4 color;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::Begin("Color Setting");
+		ImGui::ColorEdit3("Board", (float*)&color);
+		ImGui::End();
+		ImGui::Render();
+
+		processInput(window);
+
+		for (int i = 0; i < 12; i++) {
+			vertices[i * 6 + 3] = color.x;
+			vertices[i * 6 + 4] = color.y;
+			vertices[i * 6 + 5] = color.z;
+		}
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_LINES, 4, 2);
+		glDrawArrays(GL_POINTS, 6, 6);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwMakeContextCurrent(window);
+		glfwSwapBuffers(window);
+
+	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
